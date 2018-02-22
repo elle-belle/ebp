f834369d8c67df36a34f4862f8a6f36baa5edead
Alsidneio Bell <alsidneio@gmail.com>
2018-02-21 23:05:49 -0800

    node app running succesfully

---
+++ underConstruction/free-coming-soon-page-master/app.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var app = express();
+var path = require('path');
+
+//viewing on local host to test 
+
+app.use(express.static('public'));
+
+app.get('/', function(req,res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(8080);
+
